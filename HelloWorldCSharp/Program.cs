e38bcc1d8ecd8b4c166614c36d08e3c378f8eb51
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-03-22 11:02:43 +0530

    Add project files.

---
+++ HelloWorldCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
